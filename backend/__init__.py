2b8b6c7cf2b91b689f0336050fa5fcd8ea26c381
Riadh <riadh.azzoun@hotmail.com>
2023-03-26 02:26:33 +0100

    Update Added database folder creation if it doesn't exist Optimized backend imports

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,8 @@
 # Other modules
-import webview
+import os
 from pathlib import Path
 # Flask
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # Flask App
@@ -10,16 +10,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATE_DIRS = BASE_DIR / 'frontend/templates'
 STATIC_DIRS = BASE_DIR / 'frontend/static'
+DATABASE_DIR = BASE_DIR / 'database'
 
 app = Flask(__name__, static_folder=STATIC_DIRS, template_folder=TEMPLATE_DIRS)
 app.app_context().push()
 
 # App Configs
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR}/backend/database/hangman.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_DIR}/hangman.db"
 app.config['SECRET_KEY'] = 'SECRET-KEY-HERE'
 
 # Database manager
 db = SQLAlchemy(app)
+if not os.path.exists(DATABASE_DIR):
+    os.makedirs(DATABASE_DIR)
 
 # App models
 from backend import models
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from backend import app
 if __name__ == '__main__':
     webview.create_window('Hangman', app, resizable=False, width=800, height=600)
     webview.start()
+
